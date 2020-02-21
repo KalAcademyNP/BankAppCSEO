603221d3ac0b850dab33bf3b939f15d4cba18652
Kal Viswanathan <Kalpanav@microsoft.com>
2020-02-21 13:00:02 -0800

    Database

---
+++ BankApp/BankContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    class BankContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BankDb;Integrated Security=True;Connect Timeout=30;");
+        }
+
+
+    }
+}
